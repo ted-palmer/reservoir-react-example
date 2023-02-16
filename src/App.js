1d5858ec8d9c3682a082eb9d0176eec6ed01539a
Ted Palmer <etpalmer17@gmail.com>
2023-02-16 16:32:18 -0500

    Configured ReservoirKit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import {
+  ReservoirKitProvider,
+  darkTheme,
+  BuyModal,
+} from '@reservoir0x/reservoir-kit-ui'
+import { WagmiConfig, createClient } from 'wagmi'
+import { getDefaultProvider } from 'ethers'
+
+const client = createClient({
+  autoConnect: true,
+  provider: getDefaultProvider(),
+})
+
+const theme = darkTheme({
+  headlineFont: 'Sans Serif',
+  font: 'Serif',
+  primaryColor: '#323aa8',
+  primaryHoverColor: '#252ea5',
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ReservoirKitProvider
+      options={{
+        chains: [
+          {
+            id: 1,
+            baseApiUrl: 'https://api.reservoir.tools',
+            default: true,
+            apiKey: 'YOUR_KEY',
+          },
+        ],
+        source: 'YOUR_SOURCE',
+      }}
+      theme={theme}
+    >
+      <WagmiConfig client={client}>
+        <div className="App">
+          <h1>Reservoir with React</h1>
+          <BuyModal
+            trigger={<button>Buy Token</button>}
+            collectionId="0xf5de760f2e916647fd766b4ad9e85ff943ce3a2b"
+            tokenId="1236715"
+            onPurchaseComplete={(data) => console.log('Purchase Complete')}
+            onPurchaseError={(error, data) =>
+              console.log('Transaction Error', error, data)
+            }
+            onClose={(data, stepData, currentStep) =>
+              console.log('Modal Closed')
+            }
+          />
+        </div>
+      </WagmiConfig>
+    </ReservoirKitProvider>
+  )
 }
 
-export default App;
+export default App
